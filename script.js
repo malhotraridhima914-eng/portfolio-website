885eb9ac00861d65a4a7f9cbdaad5e5fc49e6729
malhotraridhima914-eng <malhotraridhima914@gmail.com>
2026-06-19 13:56:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // Prevent browser hash from scrolling to About after refresh
-window.addEventListener('load', () => {
-  if (location.hash) {
-    history.replaceState(null, '', window.location.pathname + window.location.search);
-  }
-  window.scrollTo(0, 0);
-});
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
 
